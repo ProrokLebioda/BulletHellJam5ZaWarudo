72db4e162a370b3a9eaf9b818a8a9c6cd890f571
DESKTOP-QDBPML1\Patryk <komosa.patryk@gmail.com>
2024-05-11 12:35:53 +0200

    Enabled rudimentary shooting of laser

--- Scenes/Ship/projectiles/projectile.gd
+++ Scenes/Ship/projectiles/projectile.gd
@@ -3,13 +3,15 @@ class_name ProjectileBase
 
 var direction : Vector2 = Vector2()
 @onready var lifetime_timer : Timer = Timer.new()
-@export var speed : float = 100
-@export var projectile_lifetime : float = 4
+@export var speed : float = 600
+@export var projectile_lifetime : float = 2
 @export var projectile_damage : int = 2
 
 func _ready():
-	get_tree().add_child(lifetime_timer)
+	#get_tree().add_child(lifetime_timer)
+	add_child(lifetime_timer)
 	lifetime_timer.connect("timeout", _on_lifetime_timer_timeout)
+	lifetime_timer.start(projectile_lifetime)
 
 func _physics_process(delta):
 	position += direction * speed * delta
