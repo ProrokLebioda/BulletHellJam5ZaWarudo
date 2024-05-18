daf9fdfd93d9eae691b613d60d46559b6c0baded
DESKTOP-QDBPML1\Patryk <komosa.patryk@gmail.com>
2024-05-18 15:41:31 +0200

    Added check if last boss died If yes then go to GameCompleted MVP done

--- Scenes/Enemies/bosses/simple_boss.gd
+++ Scenes/Enemies/bosses/simple_boss.gd
@@ -2,12 +2,16 @@ extends Node2D
 class_name BossBase
 @onready var path_follow_2d = $Paths/Path2D/PathFollow2D
 
+@export var is_last_boss : bool = false
+
 signal enemy_shoot(pos : Vector2, dir : Vector2, projectile: ProjectileBase)
 
 func _ready():
+	is_last_boss = true
 	if path_follow_2d.get_child(0):
 		for boss in path_follow_2d.get_children():
 			boss.connect("boss_shoot", _on_boss_body_boss_shoot)
+			boss.connect("boss_died",_on_boss_died)
 
 func _physics_process(delta):
 	progress_path(delta)
@@ -17,3 +21,8 @@ func progress_path(delta):
 
 func _on_boss_body_boss_shoot(pos, dir, projectile):
 	enemy_shoot.emit(pos, dir, projectile)
+
+func _on_boss_died():
+	if is_last_boss:
+		get_tree().change_scene_to_file("res://UI/game_complete.tscn")
+	call_deferred("queue_free")
