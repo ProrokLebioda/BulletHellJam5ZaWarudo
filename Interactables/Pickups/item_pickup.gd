05456cf70aee438cf68c910fec0d115325b2ffd0
DESKTOP-QDBPML1\Patryk <komosa.patryk@gmail.com>
2024-05-18 14:10:50 +0200

    Added movement to pickables They are purged when hitting purge zone

--- Interactables/Pickups/item_pickup.gd
+++ Interactables/Pickups/item_pickup.gd
@@ -1,3 +1,12 @@
 extends Area2D
 class_name ItemPickup
 var is_picked_up : bool = false
+var direction : Vector2 = Vector2.DOWN
+@export var speed : float = 2000
+func _physics_process(delta):
+	position += direction * Player.scroll_speed * speed * delta
+	pass
+
+
+func purge():
+	queue_free()
